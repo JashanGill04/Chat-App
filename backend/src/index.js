06ebe11b82e49d5172824d9ac724e20d5f756ee4
Jashan Gill <jpsgill6@gmail.com>
2025-06-28 15:47:29 +0530

    fixed issues

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,6 +18,12 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
 app.use(
   cors({
     origin: "http://localhost:5173",
