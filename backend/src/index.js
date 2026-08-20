4156e9d1fc807cbe46a9bd431c5169081c3b1fce
Jashanpreet Singh <jpsgill6@gmail.com>
2026-08-20 21:08:23 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,14 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
+import promClient from "prom-client";
+
+
 
 dotenv.config();
+
+const register = new promClient.Registry();
+const METRICS_TOKEN = process.env.METRICS_TOKEN;
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
@@ -25,6 +31,32 @@ app.use(
     credentials: true,
   })
 );
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'UP',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime()
+  });
+});
+
+const httpRequestDuration = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  registers: [register]
+});
+
+app.get('/metrics', (req, res, next) => {
+  if (req.headers['x-metrics-token'] !== METRICS_TOKEN) {
+    return res.status(401).send('Unauthorized');
+  }
+  next();
+}, async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
